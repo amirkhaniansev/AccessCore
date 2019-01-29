16da3669e1f3689cabbea3db714e010e4da6da72
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2019-01-30 01:42:47 +0400

    added some test folders

---
+++ test/MsSqlTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MsSqlTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test/MySqlTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySqlTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
